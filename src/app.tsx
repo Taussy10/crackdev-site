6c30f8d4d80d97bf82eb0a266636204db8757234
Tausif <tausif00100@gmail.com>
2025-03-06 23:58:51 +0530

    add Twitter auth

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,14 @@
-import Home from "./page/home"
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
-const App = () => {
+export default function App() {
   return (
-    <div className=" bg-[#F3F4F6]  min-h-screen">
-<h1 className="text-3xl font-bold underline  text-amber-500">
-    Hello Dude!
-  </h1>
-
-  <Home />
-  
-    </div>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './app'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+const authKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!authKey) {
+  console.log("error from main.tsx");
+throw new Error("Add your Clerk publishable key to the .env file")  
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ClerkProvider publishableKey={authKey} afterSignOutUrl= "/">
     <App />
+    </ClerkProvider>
   </StrictMode>,
 )
