30981b6f8dc1be8f8c00b82eac568ec2bed0a58b
Tausif <tausif00100@gmail.com>
2025-03-08 17:41:43 +0530

    Understood about diff methods of storing data

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,22 +3,51 @@
 // In array: [3,3,4,3,2]
 // for object: {name:"Hello",username:"@taussy10"}
 
-
+// normal way of storing data
 const data1 = [
-{
-postion: "1",
-name: "saleh",
-time: 345,
- html: 3,
- css: 4,
- js: 43,
- markdown: 43
-}
+  // if you want only one data
+  {
+    postion: "1",
+    name: "saleh",
+    time: 345,
+    html: 3,
+    css: 4,
+    js: 43,
+    markdown: 43,
+  },
+  {
+    postion: "2",
+    name: "taussy10",
+    time: 345,
+    html: 3,
+    css: 4,
+    js: 43,
+    markdown: 43,
+  },
+];
+data1[0].css;
 
-]
-data[0].css
-
-const data2 =[
-
-]
-// an array containg  objects 
+const data2 = [
+  {
+    user1: {
+      postion: "1",
+      name: "saleh",
+      time: 345,
+      html: 3,
+      css: 4,
+      js: 43,
+      markdown: 43,
+    },
+    user2: {
+      postion: "2",
+      name: "taussy10",
+      time: 345,
+      html: 3,
+      css: 4,
+      js: 43,
+      markdown: 43,
+    },
+  },
+];
+data2[0].user1.html
+// an array containg  objects
